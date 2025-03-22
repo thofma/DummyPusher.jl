bb5139163c4e54d0a81dcd118f8e697d85ead928
Tommy Hofmann <thofma@gmail.com>
2025-03-22 17:13:20 +0100

    asd

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,10 @@ using Test
 @testset "DummyPusher.jl" begin
     # Write your tests here.
 end
+
+@info "Running writer"
+x = rand(1:10)
+open("testfile", "w") do io
+  println(io, x)
+end
+@info "done"
